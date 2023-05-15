83cf90d2c2ef3b897b8b10dba39844be871a86ba
Rory Schram <rschram01@gmail.com>
2023-05-15 19:36:42 +0200

    Added things

--- Rory/main/main.ino
+++ Rory/main/main.ino
@@ -261,7 +261,7 @@ void loop() {
         return;
     }
 
-    String filename = String("/picture_") + counter + ".jpg";
+    String filename = String("/images/") + counter + ".jpg";
 
     if (cam.saveTo(SD_MMC, filename)) {
         Serial.println(filename + " saved to disk");
@@ -276,17 +276,60 @@ void loop() {
 }
 
 
-// HTML & CSS contents which display on web server
-String HTML = "<!DOCTYPE html>\
-<html>\
-<body>\
-<h1>My First Web Server with ESP32 - Station Mode &#128522;</h1>\
-</body>\
-</html>";
-
 // Handles what happens if a client connects to the root directory
 void handleRoot() {
-  server.send(200, "text/html", HTML);
+  /* SD_MMC pertains to the sd card "memory". It is save as a
+    variable at the same address given to fs in the fs library
+    with "FS" class to enable the file system wrapper to make
+    changes on the sd cards memory */
+  fs::FS &fs = SD_MMC;
+  String path = server.uri(); //saves the to a string server uri ex.(192.168.100.110/edit server uri is "/edit")
+  Serial.print("path ");  Serial.println(path);
+
+  //To send the index.html when the serves uri is "/"
+  if (path.endsWith("/")) {
+    path += "index.html";
+  }
+
+  //gets the extension name and its corresponding content type
+  String contentType = getContentType(path);
+  Serial.print("contentType ");
+  Serial.println(contentType);
+  File file = fs.open(path, "r"); //Open the File with file name = to path with intention to read it. For other modes see <a href="https://arduino-esp8266.readthedocs.io/en/latest/filesystem.html" style="font-size: 13.5px;"> https://arduino-esp8266.readthedocs.io/en/latest/...</a>
+  size_t sent = server.streamFile(file, contentType); //sends the file to the server references from <a href="https://github.com/espressif/arduino-esp32/blob/master/libraries/WebServer/src/WebServer.h" style="font-size: 13.5px;"> https://arduino-esp8266.readthedocs.io/en/latest/...</a>
+  file.close(); //Close the file
+}
+
+//This functions returns a String of content type
+String getContentType(String filename) {
+  if (server.hasArg("download")) { // check if the parameter "download" exists
+    return "application/octet-stream";
+  } else if (filename.endsWith(".htm")) { //check if the string filename ends with ".htm"
+    return "text/html";
+  } else if (filename.endsWith(".html")) {
+    return "text/html";
+  } else if (filename.endsWith(".css")) {
+    return "text/css";
+  } else if (filename.endsWith(".js")) {
+    return "application/javascript";
+  } else if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".gif")) {
+    return "image/gif";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".ico")) {
+    return "image/x-icon";
+  } else if (filename.endsWith(".xml")) {
+    return "text/xml";
+  } else if (filename.endsWith(".pdf")) {
+    return "application/x-pdf";
+  } else if (filename.endsWith(".zip")) {
+    return "application/x-zip";
+  } else if (filename.endsWith(".gz")) {
+    return "application/x-gzip";
+  }
+  return "text/plain";
 }
 
 void handleDelete() {
